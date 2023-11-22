74bafe744b8f5a824a5ab875336fd5efbdcbae05
YBC1837207634 <1837207634@qq.com>
2023-11-22 13:59:45 +0800

    加入管理系统

--- blog-manage/src/main/java/com/gong/blog/manage/ManageApplication.java
+++ blog-manage/src/main/java/com/gong/blog/manage/ManageApplication.java
@@ -1,10 +1,8 @@
 package com.gong.blog.manage;
 
-import com.gong.blog.common.handler.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @ComponentScan({"com.gong.blog.common", "com.gong.blog.manage"})
